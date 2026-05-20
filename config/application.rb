d36fb3c1803f69adc312197da2bf49c44b073e80
piyupi2222-alt <piyupi2222@gmail.com>
2026-05-20 11:03:40 +0900

    rr
    Renamed module from 'Rr' to 'ScheduleApp' and updated time zone configuration comments.

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Rr
+module ScheduleApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.2
@@ -20,10 +20,8 @@ module Rr
     #
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
-    # 
-    config.time_zone = "Tokyo"
-    config.active_record.default_timezone = :local
-   
+    #
+    # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
